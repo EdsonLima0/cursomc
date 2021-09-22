984d37592b6f858a1eff22f2923e33b7b76cfb72
EdsonLima0 <edson.s.lima@gmail.com>
2021-09-22 20:03:57 +0100

    Criando Repository e Service para Categoria

--- src/main/java/com/edsonslima/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/edsonslima/cursomc/resources/CategoriaResource.java
@@ -3,11 +3,15 @@ package com.edsonslima.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.edsonslima.cursomc.domain.Categoria;
+import com.edsonslima.cursomc.services.CategoriaService;
 
 
 
@@ -15,17 +19,13 @@ import com.edsonslima.cursomc.domain.Categoria;
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public List<Categoria> listar() {
-		
-		Categoria cat1 = new Categoria (1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
-		
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value="/{id}",method = RequestMethod.GET)
+	public ResponseEntity<?> buscar(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+	
 	}
 }
---
+++ src/main/java/com/edsonslima/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.edsonslima.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.edsonslima.cursomc.domain.Categoria;
+import com.edsonslima.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) { 
+		 Optional<Categoria> obj = repo.findById(id); 
+		return obj.orElse(null); 
+	}
+
+}
